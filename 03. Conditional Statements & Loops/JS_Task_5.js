46ea060a02b7061ab0a3b4b175424fba6295580d
razak0000 <xquiringdreamer@gmail.com>
2024-08-11 16:40:02 +0530

    Add solution for JS_Task_5.js

---
+++ 03. Conditional Statements & Loops/JS_Task_5.js
@@ -0,0 +1,8 @@
+// Write a JavaScript program that accept two integers and display the larger. 
+// Also show if the two integers are equal.
+
+/* --------------------------------------------------------------------------------- */
+
+/* YOUR CODE HERE */    
+
+/* --------------------------------------------------------------------------------- */
