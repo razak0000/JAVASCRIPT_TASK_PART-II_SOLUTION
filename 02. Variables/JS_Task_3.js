e36a4648f103c4d12acc4057d4e6e4d1d79ca8fe
razak0000 <xquiringdreamer@gmail.com>
2024-08-11 12:07:19 +0530

    Add solution for JS_Task_3.js

---
+++ 02. Variables/JS_Task_3.js
@@ -0,0 +1,10 @@
+// Write script to:
+    // a) Declare a JS variable, titled message
+    // b) Assign “Hello World” to variable message
+    // c) Display the message in alert box.
+
+/* --------------------------------------------------------------------------------- */
+
+/* YOUR CODE HERE */  
+
+/* --------------------------------------------------------------------------------- */
