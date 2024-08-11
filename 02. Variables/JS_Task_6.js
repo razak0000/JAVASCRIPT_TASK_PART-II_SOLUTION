9cb828afbc8646b91b5244122f91203db850fd27
razak0000 <xquiringdreamer@gmail.com>
2024-08-11 12:18:07 +0530

    Add solution for JS_Task_6.js

---
+++ 02. Variables/JS_Task_6.js
@@ -0,0 +1,17 @@
+// A visitor visits an online clothing store www.xyzClothing.com. 
+// Write a script to store in variables the following information:
+    // a. Visitor’s name
+    // b. Product title
+    // c. Quantity i.e. how many products a visitor wants to order
+// Show the following message in your browser: 
+// “John Doe ordered 5 T-shirt(s) on XYZ Clothing store”
+
+/* --------------------------------------------------------------------------------- */
+
+/* YOUR CODE HERE */ 
+let visitorName = "John Doe";
+let Quantity = 5;
+let Product = "T-shirt";
+alert(`${visitorName} ordered ${Quantity} ${Product}(s) on XYZ Clothing store`);
+
+/* --------------------------------------------------------------------------------- */
