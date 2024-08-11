54fa4bbfaa17b4f2e400152e976f8f358430dc47
razak0000 <xquiringdreamer@gmail.com>
2024-08-11 16:57:34 +0530

    Add solution for JS_Task_1.js

---
+++ 04. Functions/JS_Task_1.js
@@ -0,0 +1,14 @@
+// Write a function that takes first & last name and then it greets the user using his full name.
+
+/* --------------------------------------------------------------------------------- */
+
+/* YOUR CODE HERE */    
+ function greetUser(firstName, lastName) {
+    let fullName = firstName + " " + lastName;
+    alert("Hello, " + fullName + "!");
+    }
+console.log(greetUser("Mohammad","Razak"));
+/* --------------------------------------------------------------------------------- */
+
+
+
