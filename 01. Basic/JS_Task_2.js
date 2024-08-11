d412889d5b53c4dc8fb2408f7dfa71a850f09d79
razak0000 <xquiringdreamer@gmail.com>
2024-08-11 11:51:43 +0530

    Add JS_Task_2.js

---
+++ 01. Basic/JS_Task_2.js
@@ -0,0 +1,12 @@
+// Write a JavaScript program to find the area of a triangle where 
+// three sides are 5, 6, 7. 
+
+/* --------------------------------------------------------------------------------- */
+let a = 5;
+let b = 6;
+let c = 7;
+let area_triangle = (a + b + c) / 2;
+
+/* YOUR CODE HERE */  
+
+/* --------------------------------------------------------------------------------- */
