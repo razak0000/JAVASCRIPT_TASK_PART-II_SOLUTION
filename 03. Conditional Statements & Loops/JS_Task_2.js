a4e68dd36f945d6de69e80ce2b5d9ed5e58a39a4
razak0000 <xquiringdreamer@gmail.com>
2024-08-11 15:40:19 +0530

    Add solution for JS_Task_2.js

---
+++ 03. Conditional Statements & Loops/JS_Task_2.js
@@ -0,0 +1,23 @@
+// Write a program that takes input a number from user & state 
+// whether the number is positive, negative or zero.
+
+/* --------------------------------------------------------------------------------- */
+
+/* YOUR CODE HERE */ 
+let a = prompt("enter a number");
+   if(0 < a){
+    alert("number is positive");
+   }
+   else if(0 > a){
+    alert("number is negative");
+    }
+    else if(a == 0){
+        alert("number is zero");
+    }
+    else{
+        alert("invalid input");
+        }
+
+/* --------------------------------------------------------------------------------- */
+
+
