c7b9ca8564ce227b0890fd323a82308eb8baae17
razak0000 <xquiringdreamer@gmail.com>
2024-08-11 16:35:58 +0530

    Add solution for JS_Task_4.js

---
+++ 03. Conditional Statements & Loops/JS_Task_4.js
@@ -0,0 +1,8 @@
+// Write a JavaScript conditional statement to find the largest of five numbers. 
+// Display an alert box to show the results.
+
+/* --------------------------------------------------------------------------------- */
+
+/* YOUR CODE HERE */    
+
+/* --------------------------------------------------------------------------------- */
