23ea0e97257c7a0de8775a0022eabcced7727ae8
razak0000 <xquiringdreamer@gmail.com>
2024-08-11 18:42:17 +0530

    Add solution for JS_Task_5.js

---
+++ 04. Functions/JS_Task_5.js
@@ -0,0 +1,10 @@
+// Write a JavaScript function that generates a string ID (specified length) 
+// of random characters.
+
+// Sample character list : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+
+/* --------------------------------------------------------------------------------- */
+
+/* YOUR CODE HERE */    
+
+/* --------------------------------------------------------------------------------- */
