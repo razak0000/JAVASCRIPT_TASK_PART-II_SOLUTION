104e83232af70092699066c10892eb188057a4b1
razak0000 <xquiringdreamer@gmail.com>
2024-08-11 19:20:37 +0530

    Add solution for JS_Task_6.js

---
+++ 04. Functions/JS_Task_6.js
@@ -0,0 +1,10 @@
+// Write a JavaScript function to extract unique characters from a string.
+
+// Example string : "thequickbrownfoxjumpsoverthelazydog"
+// Expected Output : "thequickbrownfxjmpsvlazydg"
+
+/* --------------------------------------------------------------------------------- */
+
+/* YOUR CODE HERE */    
+
+/* --------------------------------------------------------------------------------- */
