65834e95fa6a22abe197910147b9a32bbe5da5f5
razak0000 <xquiringdreamer@gmail.com>
2024-08-11 18:32:19 +0530

    Add solution for JS_Task_3.js

---
+++ 04. Functions/JS_Task_3.js
@@ -0,0 +1,17 @@
+// Create 2 functions that calculate properties of a circle, using the definitions here.
+// Create a function called calcCircumference:
+    // • Pass the radius to the function.
+    // • Calculate the circumference based on the radius, and output
+        // "The circumference is NN".
+// Create a function called calcArea:
+    // • Pass the radius to the function.
+    // • Calculate the area based on the radius, and output 
+        // "The area is NN".
+// Circumference of circle = 2πr
+// Area of circle = πr2
+
+/* --------------------------------------------------------------------------------- */
+
+/* YOUR CODE HERE */    
+
+/* --------------------------------------------------------------------------------- */
