deec6bb682e7350af008c01e971e25a145628ac5
razak0000 <xquiringdreamer@gmail.com>
2024-08-13 19:30:28 +0530

    Add  solution for JS_Task_10.js

---
+++ 04. Functions/JS_Task_10.js
@@ -0,0 +1,10 @@
+// Write a JavaScript function that returns a string that has letters in alphabetical order.
+// Example string : 'webmaster'
+// Expected Output : 'abeemrstw'
+// Assume punctuation and numbers symbols are not included in the passed string.
+
+/* --------------------------------------------------------------------------------- */
+
+/* YOUR CODE HERE */    
+
+/* --------------------------------------------------------------------------------- */
