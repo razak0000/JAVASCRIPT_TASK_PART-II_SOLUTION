5decfbcf767e6695b6feb8f42d013ca89ab30b63
razak0000 <xquiringdreamer@gmail.com>
2024-08-17 16:37:41 +0530

    Add solution JS_Task_4.js

---
+++ 05. Functions & Loops/JS_Task_4.js
@@ -0,0 +1,11 @@
+// If the lengths of the sides of a triangle are denoted by a, b, and c, then 
+// area of triangle is given by
+// area = S(S − a)(S − b)(S − c)
+// where, S = ( a + b + c ) / 2
+// Calculate area of triangle using 2 functions.
+
+/* --------------------------------------------------------------------------------- */
+
+/* YOUR CODE HERE */    
+
+/* --------------------------------------------------------------------------------- */
