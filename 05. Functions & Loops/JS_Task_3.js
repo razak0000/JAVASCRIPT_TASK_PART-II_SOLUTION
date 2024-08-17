894dca1f75bb4e798a7bfe35a4c0bf21adfa55b5
razak0000 <xquiringdreamer@gmail.com>
2024-08-17 16:35:21 +0530

    Add solution JS_Task_3.js

---
+++ 05. Functions & Loops/JS_Task_3.js
@@ -0,0 +1,12 @@
+// A cashier has currency notes of denominations 10, 50 and 100. 
+// If the amount to be withdrawn is input through the keyboard in hundreds, 
+// find the total number of currency notes of each denomination the cashier will 
+// have to give to the withdrawer.
+
+// Example: Amount - 470. You will have 4 hundred notes 1 fifity notes 2 ten notes.
+
+/* --------------------------------------------------------------------------------- */
+
+/* YOUR CODE HERE */    
+
+/* --------------------------------------------------------------------------------- */
