bb84236d9dbe1f03d5b087f953b888958a13a9d0
razak0000 <xquiringdreamer@gmail.com>
2024-08-17 16:19:01 +0530

    Add solution 05 JS_Task_2.js

---
+++ 05. Functions & Loops/JS_Task_2.js
@@ -0,0 +1,11 @@
+// Write a function to delete all vowels from a sentence. Assume that the sentence 
+// is not more than 25 characters long.
+
+/* --------------------------------------------------------------------------------- */
+
+/* YOUR CODE HERE */    
+
+/* --------------------------------------------------------------------------------- */
+
+
+
