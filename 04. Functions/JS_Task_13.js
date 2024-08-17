6116c60208be417afcd59910e7e8be2e06e7b00c
razak0000 <xquiringdreamer@gmail.com>
2024-08-17 16:13:07 +0530

    Add solution for JS_Task_13.js

---
+++ 04. Functions/JS_Task_13.js
@@ -0,0 +1,10 @@
+// Write a JavaScript function that accepts a string as a parameter and find the longest 
+// word within the string.
+    // EXAMPLE STRING : 'Web Development Tutorial'
+    // EXPECTED OUTPUT : 'Development'
+
+/* --------------------------------------------------------------------------------- */
+
+/* YOUR CODE HERE */    
+
+/* --------------------------------------------------------------------------------- */
