3d0653cf256216f5ba57cf222f18a9f9c2d8133e
razak0000 <xquiringdreamer@gmail.com>
2024-08-18 22:10:26 +0530

    Add solution JS_Task_2.js

---
+++ 08. String Methods/JS_Task_2.js
@@ -0,0 +1,15 @@
+// Write a program to find the index of letter “n” in the word 
+// “India” and display the result in your browser.
+
+/* --------------------------------------------------------------------------------- */
+
+/* YOUR CODE HERE */    
+let word = "India";
+let index = word.indexOf("n");
+console.log(index);
+
+
+/* --------------------------------------------------------------------------------- */
+
+
+
