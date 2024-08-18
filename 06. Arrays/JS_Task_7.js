371d3934a48f816d849d3774efcf6f739600b6d0
razak0000 <xquiringdreamer@gmail.com>
2024-08-18 11:42:09 +0530

    Add solution JS_Task_7.js

---
+++ 06. Arrays/JS_Task_7.js
@@ -0,0 +1,14 @@
+// Write a JavaScript function to find an array containing a specific element.
+
+/* 
+Test data :
+arr = [2, 5, 9, 6];
+console.log(contains(arr, 5));
+[True] 
+*/
+
+/* --------------------------------------------------------------------------------- */
+
+/* YOUR CODE HERE */    
+
+/* --------------------------------------------------------------------------------- */    
