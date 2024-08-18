5e6d5d67c39493f65291e171af3c9354a90db411
razak0000 <xquiringdreamer@gmail.com>
2024-08-18 10:47:11 +0530

    Add solution JS_Task_4.js

---
+++ 06. Arrays/JS_Task_4.js
@@ -0,0 +1,10 @@
+// Write a program to create a single string from the below mentioned array:
+// var arr = [“This ”, “ is ”, “ my ”, “ cat”];
+// (Use array’s join method)
+
+/* --------------------------------------------------------------------------------- */
+
+/* YOUR CODE HERE */    
+
+/* --------------------------------------------------------------------------------- */
+
