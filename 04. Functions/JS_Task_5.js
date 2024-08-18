cc47bf2864122d72eaf99a476e036e27f97fd407
razak0000 <xquiringdreamer@gmail.com>
2024-08-18 12:05:19 +0530

    Add solution JS_Task_5.js

--- 04. Functions/JS_Task_5.js
+++ 04. Functions/JS_Task_5.js
@@ -5,6 +5,15 @@
 
 /* --------------------------------------------------------------------------------- */
 
-/* YOUR CODE HERE */    
+/* YOUR CODE HERE */   
+function generateRandomString(length) {
+    let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let result = "";
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+        }
+        return result;
+        }
+console.log(generateRandomString(7));       
 
 /* --------------------------------------------------------------------------------- */
