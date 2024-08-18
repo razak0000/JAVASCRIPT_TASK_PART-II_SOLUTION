86ddc03cb27105b7456227a930af34c471f5a5d3
razak0000 <xquiringdreamer@gmail.com>
2024-08-18 11:54:46 +0530

    Add solution JS_Task_8.js

---
+++ 06. Arrays/JS_Task_8.js
@@ -0,0 +1,13 @@
+// Write a JavaScript function to get the nth largest element from an unsorted array.
+
+/* 
+Test Data :
+console.log(nthlargest([ 43, 56, 23, 89, 88, 90, 99, 652], 4));
+89
+*/
+
+/* --------------------------------------------------------------------------------- */
+
+/* YOUR CODE HERE */    
+
+/* --------------------------------------------------------------------------------- */    
