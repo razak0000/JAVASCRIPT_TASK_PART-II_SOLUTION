17e910ba640a42990355b7de7b8ce87e7a872c93
razak0000 <xquiringdreamer@gmail.com>
2024-08-18 22:11:13 +0530

    Add solution JS_Task_3.js

---
+++ 08. String Methods/JS_Task_3.js
@@ -0,0 +1,18 @@
+// Write a program that converts a string “472” to a number 472. 
+// Display the values & types in your browser.
+
+/* --------------------------------------------------------------------------------- */
+
+/* YOUR CODE HERE */   
+let str = "472";
+let num = parseInt(str);
+console.log("String: ", str);
+console.log("Number: ", num);
+console.log("Type of String: ", typeof str);
+console.log("Type of Number: ", typeof num);
+
+
+/* --------------------------------------------------------------------------------- */
+
+
+
