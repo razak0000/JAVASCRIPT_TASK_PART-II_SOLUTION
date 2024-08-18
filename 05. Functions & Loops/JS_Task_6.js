70f86777ef2b0d2ccd3630a73c1de97a310e7e23
razak0000 <xquiringdreamer@gmail.com>
2024-08-18 10:39:26 +0530

    Add solution JS_Task_6.js

--- 05. Functions & Loops/JS_Task_6.js
+++ 05. Functions & Loops/JS_Task_6.js
@@ -5,5 +5,35 @@
 /* --------------------------------------------------------------------------------- */
 
 /* YOUR CODE HERE */    
+function convertToMeters(km) {
+    let meters = km * 1000;
+    console.log(`The distance in meters is ${meters} meters`);
+}
+
+function convertToFeet(km) {
+    let feet = km * 3280.84;
+    console.log(`The distance in feet is ${feet} feet`);
+}
+
+function convertToInches(km) {
+    let inches = km * 39370.08;
+    console.log(`The distance in inches is ${inches} inches`);
+}
+
+function convertToCentimeters(km) {
+    let centimeters = km * 100000;
+    console.log(`The distance in centimeters is ${centimeters} centimeters`);
+}
+
+function mainFunction() {
+    let km = parseFloat(prompt("Enter the distance between two cities in km:"));
+    convertToMeters(km);
+    convertToFeet(km);
+    convertToInches(km);
+    convertToCentimeters(km);
+}
+
+mainFunction();
+
 
 /* --------------------------------------------------------------------------------- */
