c30a4e995b6f5cf29871675a08398a2c72539b74
razak0000 <xquiringdreamer@gmail.com>
2024-08-18 12:00:52 +0530

    Add solution JS_Task_4.js

---
+++ 01. Basic/JS_Task_4.js
@@ -0,0 +1,16 @@
+// Write a JavaScript program to check a pair of numbers and return true if one of the 
+// numbers is 50 or if their sum is 50.  
+
+/* --------------------------------------------------------------------------------- */
+
+/* YOUR CODE HERE */  
+function checkPair(num1, num2) {
+    if (num1 === 50 || num2 === 50 || num1 + num2 === 50) {
+            return true;
+            } else {
+                return false;
+                }
+                }
+
+
+/* --------------------------------------------------------------------------------- */
