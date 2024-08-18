a5ac84138484d812e2b77950c7edde65881b24c9
razak0000 <xquiringdreamer@gmail.com>
2024-08-18 22:06:16 +0530

    Add solution JS_Task_4.js

---
+++ 07. Arrays & Loop/JS_Task_4.js
@@ -0,0 +1,18 @@
+// Write a program to print items of the following array using for loop:
+// fruits = [“apple”, “banana”, “mango”, “orange”, “strawberry”]
+
+// Elements at index 0 is apple
+// Elements at index 1 is banana
+// Elements at index 2 is mango
+// Elements at index 3 is orange
+// Elements at index 4 is strawberry
+
+/* --------------------------------------------------------------------------------- */
+
+/* YOUR CODE HERE */    
+let fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+for (let i = 0; i < fruits.length; i++) {
+    console.log(`Elements at index ${i} is ${fruits[i]}`);
+    }
+
+/* --------------------------------------------------------------------------------- */
