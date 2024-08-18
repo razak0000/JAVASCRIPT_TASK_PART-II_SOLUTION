56182faa96b61875fafb6d9eab714c0f985edc5c
razak0000 <xquiringdreamer@gmail.com>
2024-08-18 12:04:25 +0530

    Add solution JS_Task_5.js

--- 03. Conditional Statements & Loops/JS_Task_5.js
+++ 03. Conditional Statements & Loops/JS_Task_5.js
@@ -4,5 +4,17 @@
 /* --------------------------------------------------------------------------------- */
 
 /* YOUR CODE HERE */    
+let a = prompt("enter the first integer");
+let b = prompt("enter the second integer");
+if (a > b) {
+    alert(`${a} is greatest number `)
+    } 
+    else if (a < b) {
+        alert(`${b} is greatest number `);
+    } 
+    else {
+        alert(`both numbers are equal`);
+        }
+
 
 /* --------------------------------------------------------------------------------- */
