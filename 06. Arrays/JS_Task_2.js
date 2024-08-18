88ac725a8c1b3750597e714a459865f4d86b7a8c
razak0000 <xquiringdreamer@gmail.com>
2024-08-18 12:10:14 +0530

    Add solution JS_Task_7.js

--- 06. Arrays/JS_Task_2.js
+++ 06. Arrays/JS_Task_2.js
@@ -15,5 +15,28 @@
 /* --------------------------------------------------------------------------------- */
 
 /* YOUR CODE HERE */    
+let colors = ["red", "green", "blue", "yellow", "purple", "orange" ];
+console.log(colors);
+let addColor = prompt("Enter a color to add to the beginning of the array: ");
+colors.unshift(addColor);
+console.log(colors);
+let addColor2 = prompt("Enter a color to add to the end of the array: ");
+colors.push(addColor2);
+console.log(colors);
+colors.unshift("pink", "black");
+console.log(colors);
+colors.shift();
+console.log(colors);
+colors.pop();
+console.log(colors);
+let addColor3 = prompt("Enter a color to add at a specific index: ");
+let addIndex = prompt("Enter the index where you want to add the color: ");
+colors.splice(addIndex, 0, addColor3);
+console.log(colors);
+let deleteColor = prompt("Enter the index where you want to delete the color: ");
+let deleteCount = prompt("Enter the number of colors you want to delete: ");
+colors.splice(deleteColor, deleteCount);
+console.log(colors);
+
 
 /* --------------------------------------------------------------------------------- */
