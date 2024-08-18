0ca73228151847c8a9a0dac4b663ea8f1a06f475
razak0000 <xquiringdreamer@gmail.com>
2024-08-18 21:52:03 +0530

    Add solution JS_Task_12.js

---
+++ 06. Arrays/JS_Task_12.js
@@ -0,0 +1,15 @@
+// Write a program to initialize an array with city names.
+// Copy 3 array elements from cities array to selectedCities array.
+
+/* --------------------------------------------------------------------------------- */
+
+/* YOUR CODE HERE */   
+let cities = ["New York", "Los Angeles", "Chicago", "Houston", "Philadelphia", "Phoenix", "San Antonio", "San Diego", "Dallas", "San Jose"];
+let selectedCities = cities.slice(0, 3);
+
+console.log("Cities array:");
+console.log(cities);
+console.log("Selected Cities array:");
+console.log(selectedCities);
+
+/* --------------------------------------------------------------------------------- */
