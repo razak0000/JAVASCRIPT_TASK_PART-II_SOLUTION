213f92132e881d34d2c4158d4f7e3c5d35b37160
razak0000 <xquiringdreamer@gmail.com>
2024-08-18 21:52:51 +0530

    Add solution JS_Task_13.js

---
+++ 06. Arrays/JS_Task_13.js
@@ -0,0 +1,16 @@
+// Create a new array. Store values one by one in such a way that you can access the values 
+// in the order in which they were stored. (FIFO-First In First Out).
+
+/* --------------------------------------------------------------------------------- */
+
+/* YOUR CODE HERE */    
+let queue = [];
+queue.push(1);
+queue.push(2);
+queue.push(3);
+console.log(queue[0]); // prints: 1
+console.log(queue[1]); // prints: 2
+console.log(queue[2]); // prints: 3
+// Output: 1 2 3
+
+/* --------------------------------------------------------------------------------- */
