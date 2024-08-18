0b8685265051b8bf6238714d161772733f5f743f
razak0000 <xquiringdreamer@gmail.com>
2024-08-18 10:40:38 +0530

    Add solution JS_Task_7.js

---
+++ 05. Functions & Loops/JS_Task_7.js
@@ -0,0 +1,9 @@
+// Write a program to calculate overtime pay of employees.
+// Overtime is paid at the rate of Rs. 12.00 per hour for every hour worked above 40 hours. 
+// Assume that employees do not work for fractional part of an hour.
+
+/* --------------------------------------------------------------------------------- */
+
+/* YOUR CODE HERE */    
+
+/* --------------------------------------------------------------------------------- */
