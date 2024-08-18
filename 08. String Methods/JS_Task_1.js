49589dfe6e3d0afb85d0805ced5cb3388b489b97
razak0000 <xquiringdreamer@gmail.com>
2024-08-18 22:09:58 +0530

    Add solution JS_Task_1.js

---
+++ 08. String Methods/JS_Task_1.js
@@ -0,0 +1,15 @@
+// Write a program that takes two user inputs for first and last name using 
+// prompt and merge them in a new variable titled fullName. Greet the user using his full name.
+
+/* --------------------------------------------------------------------------------- */
+
+/* YOUR CODE HERE */    
+let firstName = prompt("Enter your first name: ");
+let lastName = prompt("Enter your last name: ");
+let fullName = firstName + " " + lastName;
+alert("Hello, " + fullName + "!");
+
+
+/* --------------------------------------------------------------------------------- */
+
+
