2bd327716ee211dd533515ab04e67cb3f346cccc
razak0000 <xquiringdreamer@gmail.com>
2024-08-18 12:02:55 +0530

    Add solution JS_Task_4.js

--- 02. Variables/JS_Task_4.js
+++ 02. Variables/JS_Task_4.js
@@ -3,5 +3,14 @@
 /* --------------------------------------------------------------------------------- */
 
 /* YOUR CODE HERE */  
+let studentName = "Mohammad Razak A";
+let studentAge = 20;
+let studentGrade = "A+";
+let studentAddress = "coimbatore";
+let studentPhone = 1234567890;
+let studentEmail = "mohammadrazak@gmail.com";
+
+alert(`hello  ${studentName} is a ${studentAge} year old and his grade is ${studentGrade} , he is from ${studentAddress} , to contact him ${studentPhone} or ${studentEmail} `);
+
 
 /* --------------------------------------------------------------------------------- */
