6d9ef4ef84d6e2403f82b1534ca8fda23271c324
razak0000 <xquiringdreamer@gmail.com>
2024-08-18 12:12:18 +0530

    Add solution JS_Task_7.js

--- 06. Arrays/JS_Task_7.js
+++ 06. Arrays/JS_Task_7.js
@@ -10,5 +10,21 @@ console.log(contains(arr, 5));
 /* --------------------------------------------------------------------------------- */
 
 /* YOUR CODE HERE */    
+function contains(arr, target) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === target) {
+            return true;
+            }
+            }
+            return false;
+            }
+            console.log(contains([2, 5, 9, 6], 5));
+            console.log(contains([2, 5, 9, 6], 7));
+            console.log(contains([2, 5, 9, 6], 6));
+            console.log(contains([2, 5, 9, 6], 9));
+            console.log(contains([2, 5, 9, 6], 2));
+            console.log(contains([2, 5, 9, 6], 1));
+            console.log(contains([2, 5, 9, 6], 3));
+            console.log(contains([2, 5, 9, 6], 4));
 
 /* --------------------------------------------------------------------------------- */    
