6ae0f4b7985ab0bdd9ef5a0c7990c65516aa6215
razak0000 <xquiringdreamer@gmail.com>
2024-08-18 10:45:44 +0530

    Add solution JS_Task_2.js

---
+++ 06. Arrays/JS_Task_2.js
@@ -0,0 +1,19 @@
+// Initialize an array with color names. Display the array elements in your browser.
+    // a. Ask the user what color he/she wants to add to the beginning & add that 
+    // color to the beginning of the array. Display the updated array in your browser.
+    // b. Ask the user what color he/she wants to add to the end & add that color to the 
+    // end of the array. Display the updated array in your browser.
+    // c. Add two more color to the beginning of the array. Display the updated array in your browser.
+    // d. Delete the first color in the array. Display the updated array in your browser.
+    // e. Delete the last color in the array. Display the updated array in your browser.
+    // f. Ask the user at which index he/she wants to add a color & color name. 
+    // Then add the color to desired position/index. Display the updated array in your browser.
+    // g. Ask the user at which index he/she wants to delete color(s) & how many colors he/she 
+    // wants to delete. Then remove the same number of color(s) from user-defined position/index. 
+    // Display the updated array in your browser.
+
+/* --------------------------------------------------------------------------------- */
+
+/* YOUR CODE HERE */    
+
+/* --------------------------------------------------------------------------------- */
