a63299b3aff75ce1ab8395ff4434757b21c36f50
razak0000 <xquiringdreamer@gmail.com>
2024-08-18 22:24:40 +0530

    Add solution JS_Task_12.js

---
+++ 08. String Methods/JS_Task_12.js
@@ -0,0 +1,11 @@
+// Write a program that takes user input. Convert and show the input in capital letters.
+
+/* --------------------------------------------------------------------------------- */
+
+/* YOUR CODE HERE */   
+let word = prompt("Enter some words");
+let capitalWord = word.toUpperCase();
+console.log(capitalWord);
+
+
+/* --------------------------------------------------------------------------------- */
