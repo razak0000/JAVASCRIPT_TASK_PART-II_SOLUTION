010b11f45614687a3bd13cefbd26706447e335bb
razak0000 <xquiringdreamer@gmail.com>
2024-08-18 12:07:00 +0530

    Add solution JS_Task_13.js

--- 04. Functions/JS_Task_13.js
+++ 04. Functions/JS_Task_13.js
@@ -6,5 +6,17 @@
 /* --------------------------------------------------------------------------------- */
 
 /* YOUR CODE HERE */    
+function findLongestWord(str) {
+    let words = str.split(' ');
+    let longestWord = '';
+    for (let i = 0; i < words.length; i++) {
+        if (words[i].length > longestWord.length) {
+            longestWord = words[i];
+            }
+            }
+        }
+
+console.log(findLongestWord("web development Tutorial"));
+
 
 /* --------------------------------------------------------------------------------- */
