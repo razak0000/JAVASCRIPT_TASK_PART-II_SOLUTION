a782dec344c0f330daa988620c722b5186fbd897
razak0000 <xquiringdreamer@gmail.com>
2024-08-18 22:43:42 +0530

    Add solution JS_Task_7.js

---
+++ 09. Date Methods/JS_Task_7.js
@@ -0,0 +1,12 @@
+// Write a program to ask the user about his age. 
+// Calculate and show his birth year in your browser.
+
+/* --------------------------------------------------------------------------------- */
+
+/* YOUR CODE HERE */    
+let age = prompt("How old are you?");
+let birthYear = 2024 - age;
+alert(`You were born in ${birthYear}`);
+
+
+/* --------------------------------------------------------------------------------- */
