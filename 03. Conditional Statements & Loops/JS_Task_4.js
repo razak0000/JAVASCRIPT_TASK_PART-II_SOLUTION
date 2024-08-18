5d048e57b44952cb378fcfc02ebf7cd4b5489eba
razak0000 <xquiringdreamer@gmail.com>
2024-08-18 12:04:02 +0530

    Add solution JS_Task_4.js

--- 03. Conditional Statements & Loops/JS_Task_4.js
+++ 03. Conditional Statements & Loops/JS_Task_4.js
@@ -3,6 +3,26 @@
 
 /* --------------------------------------------------------------------------------- */
 
-/* YOUR CODE HERE */    
+/* YOUR CODE HERE */ 
+ let num1 = 20;
+ let num2 = 10;
+ let num3 = 30;
+ let num4 = 40;
+ let num5 = 50;
+ let largest = num1;// let datatype is used because the value can be changed
+ if (num2 > largest) { 
+    largest = num2; 
+    }
+ if (num3 > largest) {
+        largest = num3;
+   }
+ if (num4 > largest) {
+        largest = num4;
+    }
+ if (num5 > largest) {
+            largest = num5
+    }
+ alert(largest); 
+
 
 /* --------------------------------------------------------------------------------- */
