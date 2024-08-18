d404d0e6358923624e8cb67aaba27f44d233a8ff
razak0000 <xquiringdreamer@gmail.com>
2024-08-18 22:28:00 +0530

    Add solution JS_Task_14.js

---
+++ 08. String Methods/JS_Task_14.js
@@ -0,0 +1,15 @@
+// Write a program to convert the following string to an array using string split method.
+// var university = “University of Madras”;
+// Display the elements of array in your browser.
+
+/* --------------------------------------------------------------------------------- */
+
+/* YOUR CODE HERE */    
+var university = "University of Madras";
+var array = university.split(" ");
+console.log(array);
+ 
+
+
+
+/* --------------------------------------------------------------------------------- */
