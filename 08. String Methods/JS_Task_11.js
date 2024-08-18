e222a7a1f768f1b0db29d8a028f04234548630a0
razak0000 <xquiringdreamer@gmail.com>
2024-08-18 22:23:32 +0530

    Add solution JS_Task_11.js

---
+++ 08. String Methods/JS_Task_11.js
@@ -0,0 +1,13 @@
+// Write a program to find the character at 3rd index in the word 
+// “India” and display the result in your browser.
+
+/* --------------------------------------------------------------------------------- */
+
+/* YOUR CODE HERE */    
+let word = "India";
+let index = 3;
+let char = word[index];
+console.log(char);
+
+
+/* --------------------------------------------------------------------------------- */
