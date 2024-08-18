dd53172fc9b22ba42923fdab638e391188d0b4f7
razak0000 <xquiringdreamer@gmail.com>
2024-08-18 10:41:55 +0530

    Add solution JS_Task_1.js

---
+++ 06. Arrays/JS_Task_1.js
@@ -0,0 +1,10 @@
+// Declare and Initialize an array and store available education qualifications 
+// in INDIA (e.g. SSC, HSC, BCA, MS, M.Phil., PhD). 
+//Show the listed qualifications in your browser.
+
+/* --------------------------------------------------------------------------------- */
+
+/* YOUR CODE HERE */    
+
+/* --------------------------------------------------------------------------------- */
+
