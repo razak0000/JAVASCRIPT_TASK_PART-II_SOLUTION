f870a6203169e91c25b66361a3e345f98bc77919
razak0000 <xquiringdreamer@gmail.com>
2024-08-18 22:40:17 +0530

    Add solution JS_Task_4.js

---
+++ 09. Date Methods/JS_Task_4.js
@@ -0,0 +1,10 @@
+// Write a program that creates a Date object for the last day of the last month 
+// of 2023 and assigns it to variable named laterDate.
+
+/* --------------------------------------------------------------------------------- */
+
+/* YOUR CODE HERE */   
+let laterDate = new Date(2023, 11, 0);
+console.log(laterDate);
+
+/* --------------------------------------------------------------------------------- */
