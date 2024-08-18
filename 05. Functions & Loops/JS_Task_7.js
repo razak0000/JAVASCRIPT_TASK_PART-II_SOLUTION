f17a1e3747eeeb82b680602dade6c51f57dceef3
razak0000 <xquiringdreamer@gmail.com>
2024-08-18 12:08:56 +0530

    Add solution JS_Task_7.js

--- 05. Functions & Loops/JS_Task_7.js
+++ 05. Functions & Loops/JS_Task_7.js
@@ -5,5 +5,23 @@
 /* --------------------------------------------------------------------------------- */
 
 /* YOUR CODE HERE */    
+function calculateOvertimePay(hoursWorked) {
+    let overtimeHours = hoursWorked - 40;
+    let overtimePay = overtimeHours * 12;
+    return overtimePay;
+}
+
+function mainFunction() {
+    let hoursWorked = parseInt(prompt("Enter the number of hours worked:"));
+    if (hoursWorked > 40) {
+        let overtimePay = calculateOvertimePay(hoursWorked);
+        console.log(`The overtime pay is Rs. ${overtimePay}`);
+    } else {
+        console.log("No overtime pay is applicable.");
+    }
+}
+
+mainFunction();
+
 
 /* --------------------------------------------------------------------------------- */
