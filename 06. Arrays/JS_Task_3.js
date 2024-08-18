6748d864a771d461603d0ae3d708cca6f4cedc40
razak0000 <xquiringdreamer@gmail.com>
2024-08-18 10:46:33 +0530

    Add solution JS_Task_3.js

---
+++ 06. Arrays/JS_Task_3.js
@@ -0,0 +1,12 @@
+// Write a program to store student scores in an array & sort the array in ascending order 
+// using Array’s sort method.
+
+// Scores of Students: 320, 230, 480, 120
+// Ordered Scores of Students: 120, 230, 320, 480
+
+/* --------------------------------------------------------------------------------- */
+
+/* YOUR CODE HERE */    
+
+/* --------------------------------------------------------------------------------- */
+
