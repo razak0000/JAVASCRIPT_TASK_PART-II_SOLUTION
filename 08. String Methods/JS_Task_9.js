dcc6a784e75a592d84a1888645c926661f658885
razak0000 <xquiringdreamer@gmail.com>
2024-08-18 22:22:23 +0530

    Add solution JS_Task_9.js

---
+++ 08. String Methods/JS_Task_9.js
@@ -0,0 +1,15 @@
+// Write a program to take a user input about his favorite mobile phone model. 
+// Find and display the length of user input in your browser.
+
+// Example: My favourite phone is: Samsung Galaxy S6 Edge Plus
+// Length of string: 28
+
+/* --------------------------------------------------------------------------------- */
+
+/* YOUR CODE HERE */    
+let user_input = prompt("Enter your favorite mobile phone model: ");
+let length = user_input.length;
+console.log("Length of string: ", length);
+
+
+/* --------------------------------------------------------------------------------- */
