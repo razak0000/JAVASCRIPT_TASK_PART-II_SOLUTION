c82da4177075492c3e874c5eec4e2d547f47b383
razak0000 <xquiringdreamer@gmail.com>
2024-08-18 22:23:05 +0530

    Add solution JS_Task_10.js

---
+++ 08. String Methods/JS_Task_10.js
@@ -0,0 +1,12 @@
+// Write a program to find the last index of letter “l” in the word “Hello World” 
+// and display the result in your browser.
+
+/* --------------------------------------------------------------------------------- */
+
+/* YOUR CODE HERE */    
+let word = "Hello World";
+let lastIndexOfL = word.lastIndexOf("l");
+console.log(lastIndexOfL);
+
+
+/* --------------------------------------------------------------------------------- */
