b16c30379cd87c19f51cdd3a33e614ebad3134c8
razak0000 <xquiringdreamer@gmail.com>
2024-08-18 10:38:30 +0530

    Add solution JS_Task_6.js

---
+++ 05. Functions & Loops/JS_Task_6.js
@@ -0,0 +1,9 @@
+// The distance between two cities (in km.) is input through the keyboard. 
+// Write four functions to convert and print this distance in 
+// meters, feet, inches and centimeters.
+
+/* --------------------------------------------------------------------------------- */
+
+/* YOUR CODE HERE */    
+
+/* --------------------------------------------------------------------------------- */
