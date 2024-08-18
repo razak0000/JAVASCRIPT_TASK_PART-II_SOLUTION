79d8de370e083d0b0ea8dd3ed33da17cc7bbba7c
razak0000 <xquiringdreamer@gmail.com>
2024-08-18 10:37:52 +0530

    Add solution JS_Task_5.js

---
+++ 05. Functions & Loops/JS_Task_5.js
@@ -0,0 +1,10 @@
+// Write a function that receives marks received by a student in 3 subjects and returns 
+// the average and percentage of these marks. there should be 3 functions one is the 
+// mainFunction and other are for average and percentage. Call those functions from 
+// mainFunction and display result in mainFunction.
+
+/* --------------------------------------------------------------------------------- */
+
+/* YOUR CODE HERE */    
+
+/* --------------------------------------------------------------------------------- */
